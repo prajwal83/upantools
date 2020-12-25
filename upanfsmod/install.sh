75659d4000cc51c589ace3c503e1eeb77bc0e2f9
Prajwala Prabhakar <srinivasa.prajwal@gmail.com>
2020-12-25 09:36:25 +0800

    update install script - libc is in upanlibs

--- upanfsmod/install.sh
+++ upanfsmod/install.sh
@@ -5,7 +5,7 @@ mkdir -p Mnt/osin
 cp ../../upanapps/out/msh Mnt/bin/
 cp ../../upanapps/out/edit Mnt/bin/
 
-cp ../../upanapps/out/libc.so Mnt/lib/
+cp ../../upanlibs/out/libc.so Mnt/lib/
 cp ../../upanapps/out/libmterm.so Mnt/lib/
 
 cp ../../upanix/osutils/.dll Mnt/osin/
